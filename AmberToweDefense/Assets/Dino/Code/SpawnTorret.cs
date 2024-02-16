48389c0b5d019488923685bd80bcdb7492f706d5
Dino <dino@gmail.com>
2024-02-15 18:54:53 -0600

    tower prototype

--- AmberToweDefense/Assets/Dino/Code/SpawnTorret.cs
+++ AmberToweDefense/Assets/Dino/Code/SpawnTorret.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnTorret : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject[] torretPlaces;
     void Start()
     {
         
